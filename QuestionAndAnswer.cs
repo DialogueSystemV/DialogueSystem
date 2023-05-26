10a46c8487ec8202a8ad907d7af733a6009ba911
Rohit685 <mittalr685@gmail.com>
2023-05-26 11:41:46 -0500

    Updated documentation

--- QuestionAndAnswer.cs
+++ QuestionAndAnswer.cs
@@ -2,9 +2,18 @@ namespace DialogueSystem;
 
 public class QuestionAndAnswer
 {
+    /// <summary>
+    /// Question that the player will ask
+    /// </summary>
     public string question { get; set; }
+    /// <summary>
+    /// The effect that the question will have on the suspect(Positive,Neutral,Negative)
+    /// </summary>
     public QuestionEffect effect { get; set; }
-        
+    
+    /// <summary>
+    /// Answer that suspect will give
+    /// </summary>
     public string answer { get; set; }
     
     public QuestionAndAnswer(string question, QuestionEffect effect, string answer)
