9b23d1c293f80da09483115ea5fa40182ab2f9cb
Rohit685 <mittalr685@gmail.com>
2023-07-17 21:42:31 -0500

    Refactored answer choosing method

--- QuestionNode.cs
+++ QuestionNode.cs
@@ -8,46 +8,76 @@ namespace DialogueSystem;
 public class QuestionNode : Node
 {
     public QuestionEffect Effect { get; set; }
-    public List<AnswerNode> PossibleAnswers { get; set; }
 
+    public List<AnswerNode> PossibleAnswers
+    {
+        get { return PossibleAnswers; }
+        set
+        {
+            PossibleAnswers = value;
+            SortProbs();
+        }
+    }
+    internal List<int> defaultProbs { get; set; }
+    internal int defaultProbMax { get; set; }
+
+    internal Random rndm = new(DateTime.Now.Millisecond);
     public QuestionNode(string Value, List<AnswerNode> possibleAnswers, QuestionEffect Effect) : base(Value)
     {
         this.Effect = Effect;
         PossibleAnswers = possibleAnswers;
+        SortProbs();
     }
     
     public QuestionNode(string Value, List<AnswerNode> possibleAnswers, QuestionEffect Effect, bool EndsConversation) : base(Value, EndsConversation)
     {
         this.Effect = Effect;
         PossibleAnswers = possibleAnswers;
+        SortProbs();
     }
     
     public QuestionNode(string Value,List<AnswerNode> possibleAnswers, Action<Ped> PerformActionIfChosen, Ped ped, QuestionEffect Effect) : base(Value, PerformActionIfChosen, ped)
     {
         this.Effect = Effect;
         PossibleAnswers = possibleAnswers;
+        SortProbs();
     }
     
     public QuestionNode(string Value,List<AnswerNode> possibleAnswers, Action<Ped> PerformActionIfChosen, Ped ped, QuestionEffect Effect, bool EndsConversation) : base(Value, PerformActionIfChosen, ped,EndsConversation)
     {
         this.Effect = Effect;
         PossibleAnswers = possibleAnswers;
+        SortProbs();
     }
     
     internal AnswerNode ChooseAnswer()
     {
         List<AnswerNode> AnswersThatMeetCondition = new List<AnswerNode>();
+        int max = -100;
         foreach (AnswerNode PA in PossibleAnswers)
         {
             if (PA.Condition != null && PA.Condition(PA.Ped))
             {
                 AnswersThatMeetCondition.Add(PA);
+                if (PA.Probability > max)
+                {
+                    max = PA.Probability;
+                }
             }
         }
-        return AnswersThatMeetCondition.Count == 0
-            ? PossibleAnswers.OrderByDescending(item => item.Probability).First()
-            : AnswersThatMeetCondition.OrderByDescending(item => item.Probability).First();
+        return AnswersThatMeetCondition.Count == 0 
+            ? PossibleAnswers[rndm.Next(PossibleAnswers.RemoveAll(item => item.Probability < defaultProbMax))]
+            : AnswersThatMeetCondition[rndm.Next(AnswersThatMeetCondition.RemoveAll(item => item.Probability < max))];
     }
-    
-    
+
+    internal void SortProbs()
+    {
+        PossibleAnswers.OrderByDescending(item => item.Probability);
+        foreach (AnswerNode PA in PossibleAnswers)
+        {
+            defaultProbs.Add(PA.Probability);
+        }
+        defaultProbMax = defaultProbs.Max();
+    }
+
 }
