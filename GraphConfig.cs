1d0ef3306a5c3f53c69d0a11592aede55b5dd9aa
Rohit685 <mittalr685@gmail.com>
2024-03-25 19:46:00 -0500

    docs: added docs to GraphConfig feat: added shouldOverwrite to AddVariable so if dev wants to replace a variable they can.

--- GraphConfig.cs
+++ GraphConfig.cs
@@ -14,7 +14,14 @@ public class GraphConfig
         this.findAndReplace = findAndReplace;
     }
 
-    public bool AddVariable(String varName, String value)
+    /// <summary>
+    /// Adds a variable to the config. 
+    /// </summary>
+    /// <param name="varName">variable that will be replaced</param>
+    /// <param name="value">the value that will replace the variable</param>
+    /// <param name="shouldOverwrite">Whether the variable should be overwritten if it already exists. Defaults to false</param>
+    /// <returns>False if the variable already exists. True if the variable was created or overwritten</returns>
+    public bool AddVariable(String varName, String value, bool shouldOverwrite = false)
     {
         try
         {
@@ -22,12 +29,21 @@ public class GraphConfig
         }
         catch (Exception e)
         {
-            Console.WriteLine(e);
+            if (e is ArgumentException && shouldOverwrite)
+            {
+                findAndReplace[varName] = value;
+                return true;
+            }
             return false;
         }
         return true;
     }
     
+    /// <summary>
+    /// Removes a variable from the config
+    /// </summary>
+    /// <param name="varName">variable to be removed</param>
+    /// <returns>False if variable was not found. True if the variable was removed</returns>
     public bool RemoveVariable(String varName)
     {
         try
@@ -36,12 +52,16 @@ public class GraphConfig
         }
         catch (Exception e)
         {
-            Console.WriteLine(e);
             return false;
         }
         return true;
     }
     
+    
+    /// <summary>
+    /// Replaces all the variables in the node with the values in the config
+    /// </summary>
+    /// <param name="node">Node that the variables will be replaced in</param>
     public void ReplaceVariables(Node node)
     {
         foreach (var kvp in findAndReplace)
